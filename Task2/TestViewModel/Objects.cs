5a32ab32a7da8fd8bb602ddf07367f37df4d87c8
carlos-plazaa <carlos.plazaa_@hotmail.com>
2022-07-04 19:08:31 +0200

    2.2.1

---
+++ Task2/TestViewModel/Objects.cs
@@ -0,0 +1,117 @@
+﻿using Service.API;
+
+namespace TestViewModel
+{
+    internal class TestingBook : IModelBook
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string author { get; set; }
+    }
+
+    internal class TestingState : IModelState
+    {
+        public int id { get; set; }
+        public int book_id { get; set; }
+        public string available { get; set; }
+    }
+
+    internal class TestingUser : IModelUser
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string surname { get; set; }
+    }
+
+    internal class TestingEvent : IModelEvent
+    {
+        public int id { get; set; }
+        public int state_id { get; set; }
+        public int user_id { get; set; }
+        public string type { get; set; }
+    }
+
+    internal class Filler
+    {
+        private List<TestingBook> testingBooks = new()
+        {
+            new TestingBook()
+            {
+                id = 1,
+                title = "a",
+                author = "a"
+            },
+            new TestingBook()
+            {
+                id = 2,
+                title = "b",
+                author = "b"
+            },
+        };
+        private List<TestingState> testingStates = new()
+        {
+            new TestingState()
+            {
+                id = 1,
+                book_id = 1,
+                available = "1"
+            },
+            new TestingState()
+            {
+                id = 2,
+                book_id = 1,
+                available = "1"
+            },
+            new TestingState()
+            {
+                id = 3,
+                book_id = 2,
+                available = "1"
+            }
+        };
+        private List<TestingUser> testingUsers = new()
+        {
+            new TestingUser()
+            {
+                id = 1,
+                name = "A",
+                surname = "A"
+            }
+        };
+        private List<TestingEvent> testingEvents = new()
+        {
+            new TestingEvent()
+            {
+                id = 1,
+                state_id = 1,
+                user_id = 1,
+                type = "Rental",
+            },
+            new TestingEvent()
+            {
+                id = 2,
+                state_id = 1,
+                user_id = 1,
+                type = "Return",
+            },
+            new TestingEvent()
+            {
+                id = 3,
+                state_id = 2,
+                user_id = 1,
+                type = "Rental",
+            },
+            new TestingEvent()
+            {
+                id = 4,
+                state_id = 3,
+                user_id = 1,
+                type = "Rental",
+            }
+        };
+        internal List<TestingBook> Books => testingBooks;
+        internal List<TestingState> States => testingStates;
+        internal List<TestingUser> Users => testingUsers;
+        internal List<TestingEvent> Events => testingEvents;
+    }
+}
---
+++ Task2/TestViewModel/Services.cs
@@ -0,0 +1,234 @@
+﻿using Presentation;
+using Service.API;
+
+namespace TestViewModel
+{
+    internal class FirstTestingService : IService
+    {
+        private List<TestingBook> books = new();
+        private List<TestingState> states = new();
+        private List<TestingUser> users = new();
+        private List<TestingEvent> events = new();
+
+        internal FirstTestingService()
+        {
+            for(int i = 1; i < 10; i++)
+            {
+                var book = new TestingBook()
+                {
+                    id = i,
+                    title = "a",
+                    author = "a"
+                };
+                books.Add(book);
+                var state = new TestingState()
+                {
+                    id = i,
+                    book_id = i,
+                    available = "1"
+                };
+                states.Add(state);
+                var user = new TestingUser()
+                {
+                    id = i,
+                    name = "a",
+                    surname = "a"
+                };
+                users.Add(user);
+                var @event = new TestingEvent()
+                {
+                    id = i,
+                    state_id = i,
+                    user_id = i,
+                    type = "Rental"
+                };
+                events.Add(@event);
+            }
+        }
+
+        public Task AddBook(int id, string title, string author)
+        {
+            var book = new TestingBook()
+            {
+                id = id,
+                title = title,
+                author = author
+            };
+            books.Add(book);
+            return Task.CompletedTask;
+        }
+
+        public Task AddEvent(int id, int state_id, int user_id, string type)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task AddState(int id, int book_id, string available)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task AddUser(int id, string name, string surname)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task DeleteBook(int id)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task DeleteEvent(int id)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task DeleteState(int id)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task DeleteUser(int id)
+        {
+            return Task.CompletedTask;
+        }
+
+        public async Task<IEnumerable<IModelBook>> GetBooks()
+        {
+            return books;
+        }
+
+        public async Task<IEnumerable<IModelEvent>> GetEvents()
+        {
+            return events;
+        }
+
+        public async Task<IEnumerable<IModelState>> GetStates()
+        {
+            return states;
+        }
+
+        public async Task<IEnumerable<IModelUser>> GetUsers()
+        {
+            return users;
+        }
+
+        public Task UpdateBook(int id, string title, string author)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task UpdateEvent(int id, int state_id, int user_id, string type)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task UpdateState(int id, int book_id, string available)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task UpdateUser(int id, string name, string surname)
+        {
+            return Task.CompletedTask;
+        }
+    }
+
+    internal class SecondTestingService : IService
+    {
+        private List<TestingBook> books = new();
+        private List<TestingState> states = new();
+        private List<TestingUser> users = new();
+        private List<TestingEvent> events = new();
+
+        internal SecondTestingService()
+        {
+
+        }
+
+        internal List<TestingBook> Books { set { books = value; } }
+        internal List<TestingState> States { set { states = value; } }
+        internal List<TestingUser> Users { set { users = value; } }
+        internal List <TestingEvent> Events { set { events = value; } }
+
+        public Task AddBook(int id, string title, string author)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task AddEvent(int id, int state_id, int user_id, string type)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task AddState(int id, int book_id, string available)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task AddUser(int id, string name, string surname)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task DeleteBook(int id)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task DeleteEvent(int id)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task DeleteState(int id)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task DeleteUser(int id)
+        {
+            return Task.CompletedTask;
+        }
+
+        public async Task<IEnumerable<IModelBook>> GetBooks()
+        {
+            return books;
+        }
+
+        public async Task<IEnumerable<IModelEvent>> GetEvents()
+        {
+            return events;
+        }
+
+        public async Task<IEnumerable<IModelState>> GetStates()
+        {
+            return states;
+        }
+
+        public async Task<IEnumerable<IModelUser>> GetUsers()
+        {
+            return users;
+        }
+
+        public Task UpdateBook(int id, string title, string author)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task UpdateEvent(int id, int state_id, int user_id, string type)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task UpdateState(int id, int book_id, string available)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task UpdateUser(int id, string name, string surname)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}

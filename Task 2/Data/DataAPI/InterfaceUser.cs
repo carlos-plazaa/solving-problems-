3f219020e8e7e356e38063b8ef4a66e9bac1411f
carlos-plazaa <carlos.plazaa_@hotmail.com>
2022-07-04 19:16:45 +0200

    2.2.2

--- Task 2/Data/DataAPI/InterfaceUser.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Data
-{ 
-    public interface InterfaceUser
-    {
-         int UserID { get; set; }
-         string Name { get; set; }
-         string Surname { get; set; }
-    }
-}
